4307636e3650439d551ef3c944bd48bd3612f734
Varmo <101868197+varmoh@users.noreply.github.com>
2023-09-06 16:27:56 +0300

    Delete Liquibase directory

--- Liquibase/analytics/changelog/1674124104-seed-user-metric-preferences.sql
+++
@@ -1,9 +0,0 @@
--- liquibase formatted sql
--- changeset gertdrui:1674124104
-INSERT INTO user_overview_metric_preference (user_id_code, metric, ordinality, active)
-SELECT DISTINCT "user".id_code,
-    metric,
-    "ordinality",
-    "ordinality" <= 6
-FROM unnest(enum_range(NULL::overview_metric)) WITH ordinality metric
-    CROSS JOIN "user";
