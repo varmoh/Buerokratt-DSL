926827431a747668f2cfae22b8ddb9b3d98a2746
Varmo <101868197+varmoh@users.noreply.github.com>
2023-09-06 18:44:36 +0300

    Update copy_directories.sh

--- copy_directories.sh
+++ copy_directories.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Define the directory structure
-APP_DIRS="/Ruuter/private/v2 /Ruuter/public/v2 /Ruuter/private/v1 /Ruuter/public/v1 /Resql /DataMapper /Liquibase /OpenSearch /Opensearch"
+APP_DIRS="/Ruuter/private/v2 /Ruuter/public/v2 /Ruuter/private/v1 /Ruuter/public/v1 /Resql /DataMapper /Liquibase /OpenSearch /OpenSearch2"
 
 for dir in $APP_DIRS; do
     if [ -d "$dir" ]; then
