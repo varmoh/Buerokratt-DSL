9140dce3841b1601ea8c7e62ef6a471035ccc8b6
Varmo <101868197+varmoh@users.noreply.github.com>
2023-09-06 18:06:21 +0300

    Delete Liquibase directory

--- Liquibase/services/changelog/1682662304-ruuter-requests-logs.sql
+++
@@ -1,9 +0,0 @@
--- liquibase formatted sql
--- changeset baha-a:1682662304
-CREATE TABLE request_logs (
-  id BIGSERIAL PRIMARY KEY,
-  timestamp TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-  level TEXT NOT NULL,
-  statusCode INT NOT NULL DEFAULT 0,
-  content TEXT NOT NULL
-);
