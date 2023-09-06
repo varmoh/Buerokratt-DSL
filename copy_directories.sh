fd09720d1f1861a061fb1af543d6a524bcae01d6
Varmo <101868197+varmoh@users.noreply.github.com>
2023-09-06 18:44:20 +0300

    Update copy_directories.sh

--- copy_directories.sh
+++ copy_directories.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Define the directory structure
+APP_DIRS="/Ruuter/private/v2 /Ruuter/public/v2 /Ruuter/private/v1 /Ruuter/public/v1 /Resql /DataMapper /Liquibase /OpenSearch /Opensearch"
+
 for dir in $APP_DIRS; do
     if [ -d "$dir" ]; then
         echo "Copying contents of $dir..."
